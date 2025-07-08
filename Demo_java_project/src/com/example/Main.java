e909f8b74388f2dc06fd9b139801452f76df1af8
vigneshanandhamurugan <avicky21121999@gmail.com>
2025-07-08 15:30:29 +0530

    Add files via upload

---
+++ Demo_java_project/src/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.test.Helper;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main class from com.example package");
+        Helper.sayHello();
+    }
+}
---
+++ Demo_java_project/src/com/test/Helper.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class Helper {
+    public static void sayHello() {
+        System.out.println("Hello from Helper class in com.test package");
+    }
+}
